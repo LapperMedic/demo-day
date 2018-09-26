ed2792692c9e999b8317b46c2972b6cc9e695a54
LapperMedic <alexrlapp@gmail.com>
2018-09-25 21:10:36 -0500

    Added transition animations

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 import Axios from 'axios';
 import Navigo from 'navigo';
+import { tween, styler } from 'popmotion';
 import Earth from '../components/Earth';
 import Header from '../components/Header';
 import Info from '../components/Info';
@@ -21,6 +22,23 @@ function render(state){
         .get(`https://i.imgur.com/${Space(state[state.active])}`) // fetch normal-res space image
         .then(body.style.backgroundImage = `url('https://i.imgur.com/${Space(state[state.active])}')`);
     root.innerHTML = `${Header(state[state.active])} ${Earth(state[state.active])} ${Info(state[state.active])} ${Slider(state)}`
+
+    document.querySelector('#header h2').addEventListener('dblclick', (event) => { // silly animation nonsense
+        var animation = tween({
+            'from': {
+                'color': '#fff',
+                'letterSpacing': '0.08rem'
+            },
+            'to': {
+                'color': '#00f',
+                'letterSpacing': '0.7rem'
+            },
+            'duration': 2000
+        });
+        var title = styler(event.target);
+        animation.start((value) => title.set(value));
+    });
+
     router.updatePageLinks();
 }
 
