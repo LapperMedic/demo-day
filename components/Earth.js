eb144580a14a7928f005c1328ec560c925b18f37
LapperMedic <alexrlapp@gmail.com>
2018-10-15 20:11:55 -0500

    Adjusted earth title text based on new display modes

--- components/Earth.js
+++ components/Earth.js
@@ -2,12 +2,14 @@ export default function Earth(state, displayMode) {
     if (state[state.active].content != 'about') {
         if (displayMode === 0)
             return `<div id="earth" title="The Earth, if you could see ${state[state.active].title}"></div>`;
-        if (displayMode === 1)
+        else if (displayMode === 1)
             return `<div id="earth" title="The Sun, if you could see ${state[state.active].title}"></div>`;
-        if (displayMode === 2)
+        else if (displayMode === 2)
             return `<div id="earth" title="The Moon, if you could see ${state[state.active].title}"></div>`;
-        else
+        else if (displayMode === 4)
             return `<div id="earth" title="The Milky Way, if you could see ${state[state.active].title}"></div>`;
+        else
+            return `<div id="earth"></div>`;
     }
 
     else
