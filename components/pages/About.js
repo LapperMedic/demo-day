02092840a43f3a04ff2622715d641049fd3b44fd
LapperMedic <alexrlapp@gmail.com>
2018-10-17 19:05:22 -0500

    Rewrote about info section

--- components/pages/About.js
+++ components/pages/About.js
@@ -1,7 +1,9 @@
 export default `
     <p><i>See with Different Eyes</i> by Alex Lapp</p>
 
-    <p>The concept of information being hidden in plain sight has always fascinated me. Did you know that while the cornea blocks ultraviolet rays, some people with <a href="https://en.m.wikipedia.org/wiki/Aphakia" target="_blank">aphakia</a> are able to see narrow bands of UV? It’s described as a <a href="https://www.theguardian.com/science/2002/may/30/medicalscience.research" target="_blank">purple-white color</a> best visible surrounding lightning strikes. Facts like this inspired me to look into the concept of being able to see any frequency within the electromagnetic spectrum, and the results are truly interesting.</p>
+    <p>Education has always been very important in my life. But despite an eagerness to learn, I often had problems learning and retaining material in school. While learning from lectures and rote memorization may work well enough for some people, I learn best when the content is worded simply, is interactive in nature, and is presented in multiple different media. With that in mind, I've created a simple, interactive experience which I hope you'll find both educational and entertaining.</p>
+
+    <!--<p>The concept of information being hidden in plain sight has always fascinated me. Did you know that while the cornea blocks ultraviolet rays, some people with <a href="https://en.m.wikipedia.org/wiki/Aphakia" target="_blank">aphakia</a> are able to see narrow bands of UV? It’s described as a <a href="https://www.theguardian.com/science/2002/may/30/medicalscience.research" target="_blank">purple-white color</a> best visible surrounding lightning strikes. Facts like this inspired me to look into the concept of being able to see any frequency within the electromagnetic spectrum, and the results are truly interesting.</p>-->
 
     <ul>
         <li>Built in JS, with JSON, AJAX, HTML, and CSS</li>
